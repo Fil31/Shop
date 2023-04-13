a5d69d64633c3d3d2f81c39e7958500fdbf29be9
Fil31 <101242957+Fil31@users.noreply.github.com>
2023-04-13 15:21:56 +0300

    Добавьте файлы проекта.

---
+++ Shop/Shop/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Shop
+{
+    public partial class MainPage : CarouselPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new ProductViewModel();
+        }
+
+        private async void OnBuyClicked(object sender, EventArgs e)
+        {
+            var product = ((Button)sender).BindingContext as Product;
+            await DisplayAlert("Купить", $"Вы хотите купить {product.Name} за {product.Price} евро?", "Да", "Нет");
+        }
+    }
+}
---
+++ Shop/Shop/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Shop
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public decimal Price { get; set; }
+    }
+    public class ProductViewModel
+    {
+        public ObservableCollection<Product> Products { get; set; }
+
+        public ProductViewModel()
+        {
+            Products = new ObservableCollection<Product>
+        {
+            new Product { Name = "iPhone 14 pro max", ImageUrl = "https://m.media-amazon.com/images/I/610pghkO81L._AC_SX679_.jpg", Price = 1400 },
+            new Product { Name = "iPhone 14", ImageUrl = "https://m.media-amazon.com/images/I/61BGE6iu4AL._AC_SX679_.jpg", Price = 890 },
+            new Product { Name = "MacBook Pro 16", ImageUrl = "https://m.media-amazon.com/images/I/61lYIKPieDL._AC_SX679_.jpg", Price = 2300 }
+        };
+        }
+    }
+}
